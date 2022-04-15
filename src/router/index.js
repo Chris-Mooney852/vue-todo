bb9632e2e7bbbe71ce519a0c8f1c89eb423c4dac
Chris Mooney <cmmooney852@gmail.com>
2022-04-15 11:12:24 +1000

    Init

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/About.vue')
+    },
+    {
+      path: '/archive',
+      name: 'archive',
+      component: () => import('../views/Archive.vue')
+    }
+  ]
+})
+
+export default router
